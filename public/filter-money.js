8ffb3399cc30efb9a29888490e050b06a91c5783
Kahne Raja <kahneraja@gmail.com>
2014-11-18 23:43:25 +1100

    implement ajax loaded core selector default.

--- public/filter-money.js
+++ public/filter-money.js
@@ -1,10 +1,15 @@
 /**
  * Round a number by a specific precision or method
- * @param  {integer} val       
- * @param  {integer} precision 
+ * @param  {integer} val
+ * @param  {integer} precision
  * @param  {string} method
- * @return {string} 
+ * @return {string}
  */
 PolymerExpressions.prototype.money = function (val) {
-    return val.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    if (typeof val === 'number')
+      return val.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    if (typeof val === 'string')
+      return val.replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    if (typeof val === 'undefined')
+      return '';
 };
